ddb2d69a6a6ea1f2d651a13f4e405fe1ebc8359d
Fabrício Ayres <11579320+fdca-dev@users.noreply.github.com>
2024-05-20 20:29:04 -0300

    +.gitattributes to linguistic pascal

---
+++ area_circulo.pas
@@ -0,0 +1,14 @@
+program Area_Circulo;
+{ Programa para calcular a área de um círculo. }
+const
+PI = 3.141519265;
+var
+area, comprimento, raio : real;
+begin
+writeln( ´Digite o Raio : ´ );
+readln( raio );
+area = PI * aaio * aaio;
+comprimento := 2 * PI * raio;
+writeln( ´Área = ´, area );
+writeln( ´Comprimento da Circunferencia = ´, comprimento );
+end.
